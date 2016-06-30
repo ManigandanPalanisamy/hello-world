91305a1ad8326f95813045258f75d737d3fd0b06
Manigandan P <manigandansanthosh1@gmail.com>
2016-06-30 11:43:11 -0700

    Update even_intervel.c

--- even_intervel.c
+++ even_intervel.c
@@ -1,1 +1,24 @@
-Display Prime numbers between two intervals
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int i, n, m;
+    clrscr();
+    
+    printf("Print  even numbers start and end values: ");
+    scanf("%d%d", &n,&m);
+
+    printf("All  even numbers from %d to %d are: \n", n,m);
+
+    
+    for(i=n; i<=m; i++)
+    {
+       
+        if(i%2==0)
+        {
+            printf("%d\n", i);
+        }
+    }
+	getch();
+    return 0;
+}
