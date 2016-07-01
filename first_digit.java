c11e1b9cdc753987d8c8cabc89cf37f159aed3f0
Manigandan P <manigandansanthosh1@gmail.com>
2016-07-01 04:42:11 -0700

    Add files via upload

---
+++ first_digit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class first_digit{
+
+   public static void main(String args[]) {
+      System.out.println(Character.isDigit('c'));
+      System.out.println(Character.isDigit('5'));
+   }
+}
